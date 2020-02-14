434fe07e8416cd9aa939313b0d1752735539fdee
dfontana <dfontana@me.com>
2020-02-13 22:33:36 -0500

    New print line inserted.

--- ProjectNumberOne/src/projectnumberone/ProjectNumberOne.java
+++ ProjectNumberOne/src/projectnumberone/ProjectNumberOne.java
@@ -16,6 +16,7 @@ public class ProjectNumberOne {
      */
     public static void main(String[] args) {
         System.out.println("Project Number One");
+        System.out.println("This is a new print line");
     }
     
 }
